ca6c935eea35e42d2fc6ad0ed7a2d1d07b99fc92
DumbySquare <bunookana@gmail.com>
2020-06-09 18:24:58 -0500

    initial commit - testing fast zip creation

---
+++ zipTemplate.ts
@@ -0,0 +1,15 @@
+
+
+const fast = require('@f5devcentral/f5-fast-core');
+
+// fast.FsTemplateProvider()
+
+const provider = new fast.FsTemplateProvider('./tempSet/');
+debugger;
+
+console.log(provider);
+
+
+// const new1 = provider.fetch('hello.mst')
+debugger;
+// console.log(new1);
